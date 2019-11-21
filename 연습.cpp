84938d85a256cc7b54e1d5fce07f8cc85318b93d
mk <midaslmg94@gmail.com>
2019-11-22 01:17:44 +0900

    우선순위 큐

---
+++ 연습.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int arr[1001];
+int choice[1001] = { 0 };
+int main() {
+	
+	for (int i = 1; i <= 1000; i++) {
+		arr[i] = i;
+	} 
+	// 1000개 중 중복을 포함하여 6개 뽑기
+}
