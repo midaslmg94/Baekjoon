e2fa41dde1767c1924e970086432cc1de93cf7b5
mk <midaslmg94@gmail.com>
2019-12-28 01:49:21 +0900

    스택 - 마지막 조건을 아직 못구함(곱셈, 덧셈 부분)

--- 2504_괄호의 값.cpp
+++ 2504_괄호의 값.cpp
@@ -5,40 +5,80 @@ using namespace std;
 string str;
 stack<char>st;
 stack<char>tmp; //  올바른 괄호인지 확인하기 위한 임시 스택
-bool is_correct() {
+bool isCorrect() {
 	tmp.push(str[0]);
 	for (int i = 1; i < str.size(); i++) {
-		if (tmp.top() == '(') {
-			if (str[i] == ')') {
-				tmp.pop();
+		if (str[i] == ')') { // 닫는 괄호라면 스택은 비어있지 않고, top에는 무조건 그에 맞는 여는 괄호들이 있어야 함
+			if (tmp.empty()) {
+				return false;
 			}
-			else {
-				tmp.push(str[i]);
+			if (tmp.top() == '[' ) {// 잘못된 괄호의 예시
+				return false;
 			}
+			else if (tmp.top() == '(') {  // 괄호 쌍 일치 --> pop
+				tmp.pop(); 
+			}
+			else { tmp.push(str[i]); }
 		}
-		else if (tmp.top() == '[') {
-			if (str[i] == ']') {
-				tmp.pop();
+		else if (str[i] == ']') {
+			if (tmp.empty()) {
+				return false;
 			}
-			else {
-				tmp.push(str[i]);
+			if (tmp.top() == '(') {// 잘못된 괄호의 예시
+				return false;
 			}
+			else if (tmp.top() == '[') {// 괄호 쌍 일치 --> pop
+				tmp.pop(); 
+			} 
+			else { tmp.push(str[i]); }
+		}
+		else if (str[i] == '(' || str[i] == '['){ // 여는 괄호는 그냥 스택에 푸시
+			tmp.push(str[i]);
 		}
 	}
-	return tmp.empty();
+	return true;
 }
+
+
+
+
+
 int main() {
 	cin >> str;
-	for (int i = 0; i < str.size(); i++) {
-		st.push(str[i]);
-	}
 	// 올바른 괄호인지 확인
-	if (!is_correct()) {
-		cout << "잘못된 괄호";
+	if (!isCorrect() || !tmp.empty()) {
+		cout << "0";
 		return 0;
 	}
-	else {
-		cout << "올바른 괄호";
+	// 올바른 괄호라면 () --> 2, [] --> 3으로 치환
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == ')') {
+			if (st.top() == '(') { // ( )이므로 2로 치환
+				st.pop();
+				st.push('2');
+			}
+			else {
+				st.push(str[i]);
+			}
+		}
+		else if (str[i] == ']') {
+			if (st.top() == '[') { // [ ]이므로 3으로 치환
+				st.pop();
+				st.push('3');
+			}
+			else {
+				st.push(str[i]);
+			}
+		}
+		else {
+			st.push(str[i]);
+		}
 	}
-	// () --> 2, [] --> 3으로 치환
+	while (!st.empty()) {
+		cout << st.top() << endl;
+		st.pop();
+	}
+	
+	// 곱셈, 덧셈
+
 }
