c3d2e8dbc0de83449c24569567aadab68d96debd
mk <midaslmg94@gmail.com>
2020-01-10 01:42:26 +0900

    그리디 - 블로그 참조

--- 1541_잃어버린 괄호.cpp
+++ 1541_잃어버린 괄호.cpp
@@ -1,23 +1,36 @@
 #include<iostream>
-using namespace std;
-/* 숫자와 기호 분리
- 입력을 어떻게 받을까 : 
- '-'뒤에서부터 묶기 시작해서 새로운 '-'가나오면 닫기
- input :  54-5+12+52-65+42-2+157-5
- 처리 : 54-(5+12+52)-(65+42)-(2+157)-5
+#include<string>
+
+/*
+
+ 55-(10+245+21)-45-5-(123+124+1)
 */
-
+using namespace std;
 string str;
-int main() {    
-    cin >> str;
-    int res = 0;
-    string tmp="";
-    for (int i = 0; i < str.size(); i++) {
-        if ('0' <= str[i] && str[i]<='9') {
-            tmp += str[i];
-        }
-        else if (str[i] == '+') {
+int sum = 0;
+int main() {
+	cin >> str;
+	string tmp = "";
+	bool is_first = true;
+	bool minus = false;
+	for (int i = 0; i <= str.size(); i++) {
+		if ('0' <= str[i] && str[i] <= '9') {
+			tmp += str[i];
+		}
+		else {
+			int num = stoi(tmp);
+			if (minus) {
+				sum -= num;
+			}
+			else {
+				sum += num;
+			}
+			tmp = "";
 
-        }
-    }
+			if (str[i] == '-') {
+				minus = true;
+			}
+		}
+	}
+	cout << sum ;
 }
