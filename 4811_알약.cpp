b8d2a0c81c028c888d575d8f3ba770edbecf847f
mk <midaslmg94@gmail.com>
2020-01-11 00:53:20 +0900

    재귀, DP - 블로그 참조

--- 4811_알약.cpp
+++ 4811_알약.cpp
@@ -1,25 +1,34 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-long long recur(int one, int half) {
-	if (one == 1 && half == 0) {
-		return 1;
-	}
-	long long result = recur(one - 1, half) + recur(one, half - 1);
-
-	return result; 
-}
+long long arr[31][31];
 int n;
+long long recur(int one, int half) {
+	if (one == 0 && half == 0)
+		return 1;
+
+	if (arr[one][half] > 0) {
+		return arr[one][half]; // ¿Ö?
+	}
+
+	if (one > 0) {
+		arr[one][half] += recur(one - 1, half + 1);
+	}
+	if (half > 0) {
+		arr[one][half] += recur(one, half - 1);
+	}
+
+	return arr[one][half];
+}
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
-	cin >> n;
-	int arr[6];
-	for (int i = 0; i < 6; i++) {
-		int k;
-		cin >> arr[i];
+
+	while (true) {
+		cin >> n;
+		if (n == 0) { break; }		
+		cout << recur(n, 0)<<endl;
 	}
-	for (int i = 0; i < 6; i++) {
-		cout << arr[i] << endl;
-	}
-	
+	return 0;
 }
