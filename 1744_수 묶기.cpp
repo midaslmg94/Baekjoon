72251b6a8b285aeda76ca44a53f29e7dac8076d9
mk <midaslmg94@gmail.com>
2020-01-12 13:02:00 +0900

    그리디 - 1이 들어올 때 처리 방식을 수정하여 정답

--- 1744_수 묶기.cpp
+++ 1744_수 묶기.cpp
@@ -8,40 +8,7 @@ vector<int>neg; // 음수 배열
 long long sum; // 합
 bool is_zero = false; // 0이 있는지
 bool is_one = false; // 1이 있는지
-int p_size ,n_size;
-void sum_pos() {
-	p_size = pos.size();
-	if (p_size%2) { // 홀수개
-		if (is_one) { // 1이 있는지 확인
-			for (int i = 0; i < p_size; i += 2) {
-				sum += (pos[i] * pos[i + 1]);
-			}
-		}
-		else {
-
-		}
-	}
-	else {// 짝수개
-		if (is_one) {// 0이 있는지 확인 
-
-		}
-		else {
-
-		}
-	}
-
-}
-
-void sum_neg() {
-	n_size = neg.size();
-	if ( n_size % 2) { // 홀수개
-
-	}
-	else {// 짝수개
-
-	}
-}
-
+int p_size, n_size;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
@@ -53,8 +20,9 @@ int main() {
 		if (tmp > 0) {
 			if (tmp == 1) {
 				is_one = true;
+				sum += 1; 
 			}
-			pos.push_back(tmp);
+			else { pos.push_back(tmp); }
 		}
 		else {
 			if (tmp == 0) {
@@ -78,9 +46,37 @@ int main() {
 	sort(pos.begin(), pos.end(), greater<int>());
 	sort(neg.begin(), neg.end());
 
-	   
+	// 1처리
+	/*if(pos.size() >= 1 && pos[pos.size() - 1] == 1) { // 1이 있음
+		if (pos.size() % 2 == 0) { // 짝수개면 1은 더하기연산을 해야함. 1 1 1 1 1 의 경우?
+			sum += pos[pos.size() - 1];
+			pos.pop_back();			
+		}
+	}*/
+	// 양수 더하기
+	if (pos.size()) {
+		for (int i = 0; i < pos.size() - 1; i+=2) {
+			sum += (pos[i] * pos[i + 1]);
+		}
+		if (pos.size() % 2 != 0) { // 홀수개면 마지막 1개가 남음
+			sum += pos[pos.size() - 1];
+		}
+	}
 
-	sum_neg();
-	sum_pos();
-	cout << sum << endl;
+	// 0 처리
+	if (neg.size() >= 1 && neg[neg.size() - 1] == 0) {
+		if (neg.size() % 2 != 0) { // 홀수개면 0은 없어져야함
+			neg.pop_back();
+		}
+	}
+	// 음수 더하기
+	if (neg.size()) {
+		for (int i = 0; i < neg.size() - 1; i+=2) {
+			sum += (neg[i] * neg[i + 1]);
+		}
+		if (neg.size() % 2 != 0) { // 홀수개면 마지막 1개가 남음
+			sum += neg[neg.size() - 1];
+		}
+	}
+	cout << sum;
 }
