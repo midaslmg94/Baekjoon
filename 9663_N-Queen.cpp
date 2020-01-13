fc6511bfa6c3b3c348ad33d38ea3dcef7c810bf5
mk <midaslmg94@gmail.com>
2020-01-14 00:43:59 +0900

    백트래킹 - 이해 x

---
+++ 9663_N-Queen.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int map[15][15];
+int n;
+int cnt;
+void dfs(int y, int x) {
+	if () {
+		return;
+	}
+	map[y][x] = 1;
+	
+}
+int main() {
+	freopen("input.txt", "r", stdin);
+	cin >> n;
+	dfs(0, 0);
+}
