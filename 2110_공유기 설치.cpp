b84bc92149409ba72fe0b82cf3673b49a51db810
mk <midaslmg94@gmail.com>
2020-01-18 14:05:33 +0900

    (실패, 블로그 정답, 이해 X)이분탐색 - 파라메트릭 서치

--- 2110_공유기 설치.cpp
+++ 2110_공유기 설치.cpp
@@ -2,11 +2,27 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-/* 
+/*
 	이분 탐색 - 파라메트릭 서치
 */
 int n, c;
 vector<int>home;
+
+bool possible(int dist) {
+	int cnt = 1; //공유기의 개수
+	int prev = home[0];
+	for (int i = 1; i < n; i++) {
+		if (home[i] - prev >= dist) {// 설치 가능
+			cnt++;
+			prev = home[i];
+		}
+	}
+	if (cnt >= c) {// 공유기를 필요량 보다 더 많이 설치함
+		return true;
+	}
+	return false;
+}
+
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -21,8 +37,19 @@ int main() {
 	}
 	sort(home.begin(), home.end());
 
+	// 최소 거리 , 최대 거리
+	int left = 1, right = home[n - 1] - home[0]; 
+	int result = 0;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (possible(mid)) {
+			result = max(result, mid);
+			left = mid + 1;
+		}
+		else {
+			right = mid - 1;
+		}
 
-
-
-
+	}
+	cout << result;
 }
