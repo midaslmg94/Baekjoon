d66d5a51909624526216f7c497fc021e0f4d034a
mk <midaslmg94@gmail.com>
2020-01-19 15:39:07 +0900

    시뮬레이션 - 블로그 참조 정답코드

--- 2933_미네랄_정답.cpp
+++ 2933_미네랄_정답.cpp
@@ -1,18 +1,17 @@
-#include<iostream>
-#include<string>
-#include<queue>
+//https://luvery93.github.io/articles/2018-11/boj-2933 참조
+
+#include <iostream>
+#include <string.h>
+#include <queue>
 using namespace std;
 int r, c;
 char mineral[111][111];
-bool cluster[111][111];
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
+bool cluster[111][111]; // 내 코드에선 visit
+int dx[4] = { -1, 1 , 0 , 0 };
+int dy[4] = { 0, 0, -1, 1 };
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	// 좌우로 움직이면 c가 증감, x축
-	// 상하로 움직이면 r이 증감, y축
 	cin >> r >> c;
 	for (int i = 1; i <= r; i++) {
 		for (int j = 1; j <= c; j++) {
@@ -20,67 +19,87 @@ int main() {
 		}
 	}
 	int n;
-	cin >> n; // 막대를 던진 횟수
-	bool left = true; // 왼쪽부터 던짐 
-	/*막대기 던져서 미네랄 파괴*/
+	cin >> n;
+	bool left = true;
 	while (n--) {
 		int height;
 		cin >> height;
 		height = r - height + 1;
-		// 왼쪽 미네랄 파괴
-		if (left == true) {
-			for (int i = 1; i <= c; i++) {
-				if (mineral[height][i] == 'x') {
-					mineral[height][i] = '.';
+		if (left) { // 왼쪽에서 던짐
+			for (int j = 1; j <= c; j++) {
+				if (mineral[height][j] == 'x') {
+					mineral[height][j] = '.';
 					break;
 				}
 			}
 		}
-		// 오른쪽 미네랄 파괴
-		else {
-			for (int i = c; i >= 1; i--) {
-				if (mineral[height][i] == 'x') {
-					mineral[height][i] = '.';
+		else {// 오른쪽에서 던짐
+			for (int j = c; j >= 1; j--) {
+				if (mineral[height][j] == 'x') {
+					mineral[height][j] = '.';
 					break;
 				}
 			}
 		}
 		left = !left;
-		/*클러스터 확인*/
+
+
+		//check
 		memset(cluster, false, sizeof(cluster));
-		for (int j = 1; j <= c; j++) {
-			if (mineral[r][j] == 'x') { // 맨 밑바닥에 미네랄있는지 확인
+		for (int j = 1; j <= c; j++) { // 맨 밑부분만 bfs 탐색시작. 왜 j만 bfs 처리?
+			if (mineral[r][j] == 'x') {
 				queue<pair<int, int>>q;
-				q.push(make_pair(r, j)); // 주변에 클러스터를 확인하기 위함
+				q.push({ r,j });
 				cluster[r][j] = true;
 				while (!q.empty()) {
-					int x = q.front().first;
-					int y = q.front().second;
+					int y = q.front().first;
+					int x = q.front().second;
 					q.pop();
 					for (int k = 0; k < 4; k++) {
-						int nx = x + dx[k];
 						int ny = y + dy[k];
-						if (1 <= nx && nx <= c && 1 <= ny && ny <= r) {
-							if (cluster[nx][ny] == false && mineral[nx][ny] == 'x') {
-								cluster[nx][ny] = true;
-								q.push(make_pair(nx, ny));
-							}
+						int nx = x + dx[k];
+						if (1 <= ny && ny <= r && 1 <= nx && nx <= c && !cluster[ny][nx] && mineral[ny][nx] == 'x') {
+							cluster[ny][nx] = true;
+							q.push({ ny, nx });
 						}
 					}
 				}
 			}
 		}
-		/*밑에서 위로 확인*/
+
+		// down : 이해가 잘 안간다.
 		int down = r;
 		for (int j = 1; j <= c; j++) {
 			for (int i = r; i >= 1; i--) {
-				if (mineral[i][j] == 'x' && cluster[i][j] == false) {
+				if (mineral[i][j] == 'x' && cluster[i][j] == false) { // 밑에서 부터 올라감
 					int col_down = r - i;
+					for (int k = i + 1; k <= r; k++) {
+						if (cluster[k][j] == true) {
+							col_down = k - i - 1;
+							break;
+						}
+					}
+					if (down > col_down) {
+						down = col_down;
+					}
+				}
+			}
+		}
 
+		for (int j = 1; j <= c; j++) {
+			for (int i = r; i >= 1; i--) {
+				if (i + down <= r && down != 0 && mineral[i][j] == 'x' && cluster[i][j] == false) {
+					mineral[i + down][j] = mineral[i][j];
+					mineral[i][j] = '.';
 				}
 			}
 		}
 	}
-
+	for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			cout << mineral[i][j];
+		}
+		cout << '\n';
+	}
 
 }
