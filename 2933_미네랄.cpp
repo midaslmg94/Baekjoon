e9789dd2256a66ec5a90ba40102bc06e8dbcdfbd
mk <midaslmg94@gmail.com>
2020-01-19 13:59:05 +0900

    (실패)시뮬레이션

--- 2933_미네랄.cpp
+++ 2933_미네랄.cpp
@@ -1,63 +1,137 @@
 #include<iostream>
-#include<vector>
+#include<algorithm>
+#include<queue>
+#include<string.h>
 using namespace std;
-string arr[100];
-int r = 0;
-int c = 0;
+int r, c, n;
+char map[101][101];
+bool visit[101][101];
+int height[101];
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
-bool canGo(int x, int y) {
-	bool check = true;
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (c <= nx || nx < 0 || r <= ny || ny < 0) {
-			check = false; break;
+bool isground = false;
+void throwing(int height, bool oper) { // 막대 던지기
+	if (oper) { // 왼쪽에서 던짐
+		for (int j = 1; j <= c; j++) {
+			if (map[height][j] == 'x') {
+				map[height][j] = '.';
+				break;
+			}
 		}
 	}
-	return check;
+	else {// 오른쪽에서 던짐
+		for (int j = c; j >= 1; j--) {
+			if (map[height][j] == 'x') {
+				map[height][j] = '.';
+				break;
+			}
+		}
+	}
 }
-void goDown(int x, int y) {
-	if (canGo(x, y)) { 
+int min_dist = 999999999;
+void moving() {//visit가 true인 곳을 움직이자(밑에서 부터 훑기)
+	bool flag = false;
+	for (int i = r; i >= 1; i--) {
+		for (int j = c; j >= 1; j--) {
+			if (visit[i][j] == true && map[i][j] == 'x') {
+				// 가장 적게 떨어지는 길이가 몇인지 확인
+				int dist = 0; int cur = i;
+				//cout << "cur = " << cur<<" j = "<<j << endl;
+				while (cur < r) {
+					cur++;
+					dist++;
+					if (map[cur][j] == 'x') {
+						break;
+					}
+				}
+				min_dist = min(dist, min_dist);
+				flag = true; // 공중에 떠있는 클러스터의 가장 밑 부분을 체크
+			}
+		}
+		if (flag) { break; } 
+	}
+	//cout << "min_dist " << min_dist << endl;
+	// min_dist 만큼 미네랄 이동
+	for (int i = r; i >= 1; i--) {
+		for (int j = c; j >= 1; j--) {
+			if (visit[i][j] == true && map[i][j] == 'x') {
+				map[i][j] = '.';
+				map[i + min_dist][j] = 'x';
+			}
+		}
+	}
+	memset(visit, false, sizeof(visit));
+}
+void bfs(int y, int x) {
+	isground = false;
+	queue<pair<int, int>>q;
+	q.push({ y,x });
+	visit[y][x] = true;
+	while (!q.empty()) {
+		y = q.front().first;
+		x = q.front().second;
+		//cout << "y : " << y << " x : " << x << endl;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (1 <= ny && ny <= r && 1 <= nx && nx <= c && !visit[ny][nx] && map[ny][nx] == 'x') {
+				q.push({ ny,nx });
+				visit[ny][nx] = true;
+				if (ny == r) { // 바닥까지 미네랄이 연결 --> 공중에 떠있지 않음
+					isground = true;						
+				}
+			}
+		}
+	}
+	if (!isground) { // 여기가 한번 더 들어감
+		moving();
+	}
+}
 
+
+void destory(int height, bool oper) {
+	throwing(height, oper);//막대를 던짐	
+	/*for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			cout << map[i][j];
+		}
+		cout << "\n";
+	}
+	cout << "\n\n";*/
+
+	for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			if (map[i][j] == 'x' && !visit[i][j]) {			
+				//cout << "i = " << i << " j = " << j << endl;
+				bfs(i, j);// 클러스터 확인
+			}
+		}
 	}
 }
 int main() {
-	// 미네랄 지도 입력
+	ios::sync_with_stdio(false);
+	cin.tie(0);	cout.tie();
+	freopen("input.txt", "r", stdin);
 	cin >> r >> c;
-	for (int i = 0; i < r; i++) {
-		cin >> arr[i];
+	for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			cin >> map[i][j];
+		}
 	}
-	int n = 0; // 막대를 던진 횟수
 	cin >> n;
-	// 막대를 던진 높이, 
-	vector<int>v(n);
+	bool left = true;
+	int height = 0;
 	for (int i = 0; i < n; i++) {
-		cin >> v[i];
+		cin >> height;
+		destory(c - height + 1, left);// 높이를 배열의 인덱스로 변환, true:왼쪽, false:오른쪽
+		left = !left;
+		memset(visit, false, sizeof(visit));
 	}
-	for (int i = 0; i < n; i++) {
-		int height = v[i];
-		if (i % 2 == 0) { //짝수 인덱스 : 왼쪽->오른쪽 으로 던짐
-			for (int j = 0; j < r; j++) {
-				if (arr[r - height][j] == 'x') {
-					arr[r - height][j] = '.';
-					goDown(r - height, j);
-					break;
-				}
-			}
+	for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			cout << map[i][j];
 		}
-		else { // 홀수 인덱스 : 오른쪽->왼쪽으로 던짐 
-			for (int j = 0; j < c; j++) {
-				if (arr[r - height][c - j] == 'x') {
-					arr[r - height][c - j] = '.';
-					goDown(r - height, c - j);
-					break;
-				}
-			}
-		}
-
-	}
-	for (int i = 0; i < r; i++) {
-		cout << arr[i] << "\n";
+		cout << "\n";
 	}
 }
