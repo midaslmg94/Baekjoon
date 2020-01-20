e701e7d3750a542e5386f12d10e86ace85ea8d8f
mk <midaslmg94@gmail.com>
2020-01-21 00:15:15 +0900

    백트래킹 - prev_permutation 사용

--- 1759_암호 만들기.cpp
+++ 1759_암호 만들기.cpp
@@ -4,26 +4,53 @@
 #include<algorithm>
 using namespace std;
 int L, C;
+bool check(string str) {
+    int vowel_cnt = 0; 
+    int consonant_cnt = 0;
+    for (int i = 0; i < str.size(); i++) {
+        char tmp = str[i];
+        if (tmp == 'a' || tmp == 'e' || tmp == 'i' || tmp == 'o' || tmp == 'u') {
+            vowel_cnt++;
+        }
+        else {
+            consonant_cnt++;
+        }
+    }
+
+    if (vowel_cnt < 1 || consonant_cnt < 2)
+        return false;
+    return true;
+}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	freopen("input.txt", "r", stdin);
-	cin >> L >> C;
-	vector<char>consonant; // 자음
-	vector<char>vowel; // 모음
-	for (int i = 0; i < C; i++) {
-		char tmp;
-		cin >> tmp;
-		if (tmp == 'a' || tmp == 'e' || tmp == 'i' || tmp == 'o' || tmp == 'u') {
-			consonant.push_back(tmp);
-		}
-		else { vowel.push_back(tmp); }
-	}
-	sort(consonant.begin(), consonant.end());
-	sort(vowel.begin(), vowel.end());
-	
-	for (int i = 1; i < L-1; i++) {
-
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    freopen("input.txt", "r", stdin);
+    cin >> L >> C; // 길이 : L
+    vector<char>alpha;
+    for (int i = 0; i < C; i++) {
+        char tmp;
+        cin >> tmp;
+        alpha.push_back(tmp);
+    }
+    vector<int>select;// C개 중 L개 뽑기
+    for (int i = 1; i <= C; i++) {
+        if (i <= L)
+            select.push_back(1);
+        else {
+            select.push_back(0);
+        }
+    }
+    sort(select.begin(), select.end(), greater<int>());
+    sort(alpha.begin(), alpha.end());
+    do {
+        string str = "";
+        for (int i = 0; i < select.size(); i++) {
+            if (select[i] == 1) {
+                str+=alpha[i];
+            }            
+        }
+        if(check(str)) //  최소 한 개의 모음과 최소 두 개의 자음으로 구성되었는지 확인
+            cout <<str<< "\n";
+    } while (prev_permutation(select.begin(), select.end()));
 }
