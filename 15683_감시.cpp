86d415196a7f9960863a3f0568c3b8ed14c6bb20
mk <midaslmg94@gmail.com>
2020-02-16 17:05:37 +0900

    시뮬레이션, DFS - 코드를 참조하며 풀었는데 답이 틀린다.

--- 15683_감시.cpp
+++ 15683_감시.cpp
@@ -1,181 +1,121 @@
 #include<iostream>
+#include<vector>
 #include<string.h>
 #include<algorithm>
 #define MAX 8
+struct CCTV {
+	int y;
+	int x;
+	int type;
+};
 using namespace std;
 int n;
 int m;
-int arr[MAX][MAX];
+int map[MAX][MAX];
 
-int check_left(int y, int x) {
-	int res = 0;
-	for (int j = x-1; j>=0; j--) {
-		if (arr[y][j] == 6) {
-			break;
-		}
-		if (arr[y][j] == 0) {
-			res++;
-		}
-	}
-	return res;
-}
-int check_right(int y, int x) {
-	int res = 0;
-	for (int j = x + 1; j < m; j++) {
-		if (arr[y][j] == 6) {
-			break;
-		}
-		if (arr[y][j] == 0) {
-			res++;
-		}
-	}
-	return res;
-}
-int check_up(int y, int x) {
-	int res = 0;
-	for (int i = y-1; i >= 0; i--) {
-		if (arr[i][x] == 6) {
-			break;
-		}
-		if (arr[i][x] == 0) {
-			res++;
-		}
-	}
-	return res;
-}
-int check_down(int y, int x) {
-	int res = 0;
-	for (int i = y + 1; i < n; i++) {
-		if (arr[i][x] == 6) {
-			break;
-		}
-		if (arr[i][x] == 0) {
-			res++;
-		}
-	}
-	return res;
-}
+CCTV cctv[MAX];  // cctv는 최대 8개
+int cctv_size;
+int res = 1000;
 
-// 감시 방향으로 봄
-void left(int y, int x) {
-	for (int j = x - 1; j >= 0; j--) {
-		if (arr[y][j] == 6) {
-			break;
+void update(int dir, int y, int x) {
+	if (dir == 0) {// 상
+		for (int i = y - 1; i >= 0; i--) {
+			if (map[i][x] == 6) break;
+			if (map[i][x] == 0) 	map[i][x] = -1;
 		}
-		if (arr[y][j] == 0) {
-			arr[y][j] = -1;
+	}
+	else if (dir == 1) {// 우
+		for (int j = x + 1; j < m; j++) {
+			if (map[y][j] == 6) break;
+			if (map[y][j] == 0) map[y][j] = -1;			
+		}
+	}
+	else if (dir == 2) {// 하
+		for (int i = y + 1; i < n; i++) {
+			if (map[i][x] == 6) break;
+			if (map[i][x] == 0) map[i][x] = -1;
+		}
+	}
+	else if (dir == 3 ) {// 좌
+		for (int j = x - 1; j >= 0; j--) {
+			if (map[y][j] == 6) break;
+			if (map[y][j] == 0) map[y][j] = -1;
 		}
 	}
 }
-void right(int y, int x) {
-	for (int j = x + 1; j < m; j++) {
-		if (arr[y][j] == 6) {
-			break;
+void dfs(int cctv_idx) {
+	if (cctv_idx == cctv_size) {
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == 0) {
+					cnt++;
+				}
+			}
 		}
-		if (arr[y][j] == 0) {
-			arr[y][j] = -1;
+		res = min(res, cnt);
+		return;
+	}
+	int cctv_type = cctv[cctv_idx].type;
+	int y = cctv[cctv_idx].y;
+	int x = cctv[cctv_idx].x;
+	int backup[MAX][MAX];
+	memcpy(backup, map, sizeof(backup));
+
+	// dir[0] == 상, dir[1]==우, dir[2] ==하, dir[3]==좌
+	if (cctv_type == 1) {// 상, 하, 좌, 우로 감시 가능
+		for (int dir = 0; dir < 4; dir++) { 
+			update(dir, y, x); // 1개의 면을 각각 업데이트 하면 된다
+			dfs(cctv_idx + 1);
+			memcpy(map, backup, sizeof(map)); 
 		}
+	}
+	else if (cctv_type == 2) {// 상하, 좌우로 감시 가능
+		for (int dir = 0; dir < 2; dir++) {// 2개의 면을 업데이트 해야한다.
+			// dir==0, dir==2 일때 : 상&하 업데이트
+			// dir==1, dir==3 일때 : 좌&우 업데이트
+			update(dir, y, x); 
+			update(dir + 2, y, x);
+			dfs(cctv_idx + 1);
+			memcpy(map, backup, sizeof(map));
+		}
+	}
+	else if (cctv_type == 3) {// 상좌, 상우, 하좌, 하우 로 감시 가능
+		for (int dir = 0; dir < 4; dir++) {// 2개의 면을 업데이트 해야한다.
+			// dir==0, dir==1 일때 : 상&우 업데이트
+			// dir==1, dir==2 일때 : 우&하 업데이트
+			// dir==2, dir==3 일때 : 하&좌 업데이트
+			// dir==3, dir==0 일때 : 좌&상 업데이트
+			update(dir, y, x); 
+			update((dir + 1)%4, y, x);
+			dfs(cctv_idx + 1);
+			memcpy(map, backup, sizeof(map));
+		}
+	}
+	// dir[0] == 상, dir[1]==우, dir[2] ==하, dir[3]==좌
+
+	else if (cctv_type == 4) {// 상좌우, 상좌하, 하좌우, 상하우 로 감시 가능
+		for (int dir = 0; dir < 4; dir++) {// 3개의 면을 업데이트 해야한다.
+			// dir==0, dir==1, dir==2 일때 : 상&우&하 업데이트
+			// dir==1, dir==2, dir==3 일때 : 우&하&좌 업데이트
+			// dir==2, dir==3, dir==0 일때 : 하&좌&상 업데이트
+			// dir==3, dir==0, dir==1 일때 : 좌&상&우 업데이트
+			update(dir, y, x);
+			update((dir+1)%4, y, x);
+			update((dir+2)%4, y, x);
+			dfs(cctv_idx + 1);
+			memcpy(map, backup, sizeof(map));
+		}
+
+	}
+	else { // 상하좌우로 감시 가능
+		// 4개의 면을 업데이트 해야한다.
+		for (int dir = 0; dir < 4; dir++) {
+			update(dir, y, x);
+		}
+		dfs(cctv_idx + 1); 		
 	}
 }
-void up(int y, int x) {
-	for (int i = y - 1; i >= 0; i--) {
-		if (arr[i][x] == 6) {
-			break;
-		}
-		if (arr[i][x] == 0) {
-			arr[i][x] = -1;
-		}
-	}
-}
-void down(int y, int x) {
-	for (int i = y + 1; i < n; i++) {
-		if (arr[i][x] == 6) {
-			break;
-		}
-		if (arr[i][x] == 0) {
-			arr[i][x] = -1;
-		}
-	}
-}
-
-void sol(int y, int x, int cam) {
-	if (cam == 5) {
-		left(y, x); right(y, x); up(y, x); down(y, x);
-	}
-	else if (cam == 1) {
-		int max_cam1 = 0; 		
-		int c1 = check_up(y, x);// 상
-		int c2 = check_down(y, x);//하
-		int c3 = check_left(y, x);//좌
-		int c4 = check_right(y, x);//우
-		max_cam1 = max(c1,max(c2,max(c3, c4)));
-
-		if (max_cam1 == c1) up(y, x);
-		else if (max_cam1 == c2)down(y, x);
-		else if (max_cam1 == c3)left(y, x);
-		else right(y, x);
-	}
-
-	else if (cam == 2) {
-		int max_cam2 = 0;
-		
-		int c1 = check_up(y, x) + check_down(y, x);// 상하
-		int c2 = check_left(y, x) + check_right(y, x);// 좌우
-		max_cam2 = max(c1, c2);
-		if (max_cam2 == c1) {
-			up(y, x);
-			down(y, x);
-		}
-		else {
-			left(y, x);
-			right(y, x);
-		}
-	}
-	else if (cam == 3) {
-		int max_cam3 = 0;
-		int c1 = check_up(y, x) + check_left(y, x);// 상좌
-		int c2 = check_up(y, x) + check_right(y, x);// 상우
-		int c3 = check_down(y, x) + check_left(y, x);// 하좌
-		int c4 = check_down(y, x) + check_right(y, x);// 하우
-		max_cam3 = max(c1, max(c2, max(c3, c4)));
-		if (max_cam3 == c1) {
-			up(y, x); left(y, x);
-		}
-		else if (max_cam3 == c2) {
-			up(y, x); right(y, x);
-		}
-		else if (max_cam3 == c3) {
-			down(y, x); left(y, x);
-		}
-		else {
-			down(y, x); right(y, x);
-		}
-	}
-
-	else if (cam == 4) {
-		int max_cam4 = 0;
-		int c1 = check_up(y, x) + check_left(y, x) + check_right(y, x);// 상좌우
-		int c2 = check_up(y, x) + check_left(y, x) + check_down(y, x);// 상좌하
-		int c3 = check_left(y, x) + check_down(y, x) + check_right(y, x);// 하좌우
-		int c4 = check_up(y, x) + check_down(y, x) + check_right(y, x);// 상하우
-		max_cam4 = max(c1, max(c2, max(c3, c4)));
-
-		if (max_cam4 == c1) {
-			up(y, x); left(y, x); right(y, x);
-		}
-		else if (max_cam4 == c2) {
-			up(y, x); down(y, x); left(y, x);
-		}
-		else if (max_cam4 == c3) {
-			down(y, x); left(y, x); right(y, x);
-		}
-		else {
-			up(y, x); down(y, x); right(y, x);
-		}
-	}
-
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);	cout.tie(0);
@@ -183,32 +123,15 @@ int main() {
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] != 0 && arr[i][j] != 6) {
-				sol(i, j, arr[i][j]);
+			cin >> map[i][j];
+			if (map[i][j] != 0 && map[i][j] != 6) {
+				cctv[cctv_size].y = j;
+				cctv[cctv_size].x = i;
+				cctv[cctv_size].type = map[i][j];
+				cctv_size++;
 			}
 		}
 	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cout << arr[i][j] << ' ';
-		}
-		cout << endl;
-	}
-
-
-	// 0의 개수를 계산 
-	int res = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] == 0)
-				res++;
-		}
-	}
+	dfs(0);
 	cout << res;
 }
