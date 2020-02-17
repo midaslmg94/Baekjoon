eaa97646173bc1db67d24eb3f394e037ad7aa8b8
mk <midaslmg94@gmail.com>
2020-02-17 22:36:12 +0900

    시뮬레이션 - 조건이 많았지만 크게 어렵진 않았음

--- 17281_야구.cpp
+++ 17281_야구.cpp
@@ -7,14 +7,15 @@
 #define MAX 50
 using namespace std;
 int n;
-int player[MAX][MAX]; // 각 선수가 이닝에서 얻는 결과. 자신이 가장 좋아하는 선수인 1번 선수를 4번 타자로 미리 결정했다. 
+int player[MAX][9]; // 각 선수가 이닝에서 얻는 결과. 자신이 가장 좋아하는 선수인 1번 선수를 4번 타자로 미리 결정했다. 
 deque<int>order = { 2,3,4,5,6,7,8,9 }; // 치는 순서
 int max_score = -1;
-int ground[4];
+int ground[3]; // 1루, 2루, 3루에 주자가 존재하는지
+int score = 0;
 void hit(int info) {
 	if (info == 1) { // 1루타
-		if (ground[2]) { // 주루에 선수가 존재한다면
-			ground[3] += 1;
+		if (ground[2]) { // 주루에 선수가 존재한다면 그 위치의 타자를 1루 진루 시키고, 해당 플레이트는 비움
+			score += 1;
 			ground[2] = 0;
 		}
 		if (ground[1]) {
@@ -28,11 +29,11 @@ void hit(int info) {
 	}
 	else if (info == 2) { // 2루타
 		if (ground[2]) { // 주루에 선수가 존재한다면 그 위치의 타자를 2루 진루 시키고, 해당 플레이트는 비움
-			ground[3] += 1;
+			score += 1;
 			ground[2] = 0;
 		}
 		if (ground[1]) {
-			ground[3] += 1;
+			score += 1;
 			ground[1] = 0;
 		}
 		if (ground[0]) {
@@ -41,45 +42,51 @@ void hit(int info) {
 		}
 	}
 	else if (info == 3) {// 3루타
-		if (ground[2]) { // 주루에 선수가 존재한다면
-			ground[3] += 1;
+		if (ground[2]) { // 주루에 선수가 존재한다면 그 위치의 타자를 3루 진루 시키고, 해당 플레이트는 비움
+			score += 1;
 			ground[2] = 0;
 		}
 		if (ground[1]) {
-			ground[3] += 1;
+			score += 1;
 			ground[1] = 0;
 		}
 		if (ground[0]) {
-			ground[3] += 1;
+			score += 1;
 			ground[0] = 0;
 		}
 	}
 	else {// 홈런
-		if (ground[2]) { // 주루에 선수가 존재한다면
-			ground[3] += 1;
+		if (ground[2]) { // 주루에 선수가 존재한다면 그 위치의 타자를 4루 진루 시키고, 해당 플레이트는 비움
+			score += 1;
 			ground[2] = 0;
 		}
 		if (ground[1]) {
-			ground[3] += 1;
+			score += 1;
 			ground[1] = 0;
 		}
 		if (ground[0]) {
-			ground[3] += 1;
+			score += 1;
 			ground[0] = 0;
 		}
 	}
 }
 int game(deque<int>play) {// 타석에서는 순서
-	int score = 0;
+	score = 0;
 	int start = 0;
-	memset(ground,0,sizeof(ground)); // 1루, 2루, 3루, 홈에 들어온 주자
 	for (int i = 0; i < n; i++) {		
+		memset(ground,0,sizeof(ground)); // 1루, 2루, 3루, 홈에 들어온 주자
 		int out_count = 0;
-		while (out_count<3) {
+		while (true) {
 			int cur_player = play[start]; // 현재 타석에 들어간 타자.
-			int hit_info = player[i][cur_player-1];
+			int hit_info = player[i][cur_player-1]; // 타자가 치게 될 타수
 			if (hit_info == 0) {// 아웃
 				out_count++;
+				start++;
+				start = start % 9; // 마지막 선수까지 오면 맨 앞으로 다시
+				if (out_count == 3) {
+					break;
+				}
+				continue;
 			}
 			else if (hit_info == 1) {// 1루타
 				hit(1);
@@ -95,10 +102,8 @@ int game(deque<int>play) {// 타석에서는 순서
 			}
 			else {// 홈런
 				hit(4);
-				ground[3] += 1;
-			}
-			score += ground[3]; // 홈에 들어온 주자 추가
-			ground[3] = 0;
+				score+= 1;
+			}			
 			start++;
 			start = start % 9; // 마지막 선수까지 오면 맨 앞으로 다시
 		}
@@ -111,7 +116,7 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+		for (int j = 0; j < 9; j++) {
 			cin >> player[i][j];
 		}
 	}
@@ -124,8 +129,8 @@ int main() {
 		tmp.insert(it + 3, 1); // 4번타자에 1번선수를 넣음
 		int cur_score = game(tmp); 
 		max_score = max(max_score, cur_score);
-		//cout << "cur_score " << cur_score << "max_score " << max_score << endl;
 	} while (next_permutation(order.begin(), order.end()));
 	
-	cout << max_score;
+	cout << max_score;	
+	return 0;
 }
