32dd35fec62702c417bb4c45c11a5d2a56522ebf
minkiLee <37613051+midaslmg94@users.noreply.github.com>
2020-02-18 15:10:21 +0900

    이분탐색(실패) - 어떤 기준으로 이분탐색을 해야하는지 잘 모르겠다

---
+++ 2869_달팽이는 올라가고 싶다.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#define endl "\n"
+
+using namespace std;
+int a, b, v;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	freopen("input.txt", "r", stdin);
+	cin >> a >> b >> v;
+	
+	int answer = 0;
+	int left = 0;
+	int right = v;
+	int dist = a-b;
+	while (left<=right){
+		int mid = (left+right) / 2;
+		}
+	}
+
+	
+	cout << answer;
+}
