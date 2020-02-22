719176b07027342464045bf40d0e56da12f74e3b
mk <midaslmg94@gmail.com>
2020-02-23 01:58:12 +0900

    DP - LIS, lower_bound를 사용하여 O(nlogn) 으로 탐색

--- 1965_상자넣기.cpp
+++ 1965_상자넣기.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 int arr[MAX];
 int dp[MAX];
+vector<int>v_dp;
 int ans = 0;
 int main() {
 	ios::sync_with_stdio(false);
@@ -15,15 +16,15 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
-	dp[0] = 1;
+	v_dp.push_back(arr[0]);
 	for (int i = 1; i < n; i++) {
-		dp[i] = 1;
-		for (int j = 0; j < i; j++) {
-			if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
-				dp[i] = dp[j] + 1;
-			}
+		if (v_dp.back() < arr[i]) {//더 큰 값이 들어옴
+			v_dp.push_back(arr[i]);
 		}
-		ans = max(dp[i],ans);
+		else { // 더 작은 값이 들어옴 
+			auto pos = lower_bound(v_dp.begin(), v_dp.end(),arr[i]);
+			*pos = arr[i];
+		}
 	}
-	cout << ans;
+	cout << v_dp.size();
 }
