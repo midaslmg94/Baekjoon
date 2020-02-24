9d8fe43a61570d9141be25cd6d9a655f9634b4c3
mk <midaslmg94@gmail.com>
2020-02-24 14:17:37 +0900

    BFS (실패) - 메모리초과, 테케 오류

--- 5427_불.cpp
+++ 5427_불.cpp
@@ -1,125 +1,121 @@
 #include<iostream>
-#include<vector>
-#include<deque>
 #include<queue>
-#include<string>
 #include<string.h>
+#define endl "\n"
+#define MAX 1000
 using namespace std;
 
-int dx[4] = { 0,0,1,-1 };
-int dy[4] = { 1,-1,0,0 };
-int w,h=0;
-vector<vector<char>>map;
+struct INFO {
+	bool visit; // 방문했는지
+	bool next_fire; // 다음에 불 날 위치인가
+};
 
-int dist[1000][1000];
-deque<pair<int, int>>fire; // 불의 위치
-
-bool isAlive(int x, int y) {
+int t;
+int w, h;
+int result; // 정답
+bool visit[MAX][MAX];
+char map[MAX][MAX];
+//INFO visit[MAX][MAX];
+queue<pair<int, int>>sang;// 상근이의 위치
+queue<pair<int, int>>fire;// 불의 위치
+int dy[4] = { 0,0,1,-1 };
+int dx[4] = { 1,-1,0,0 };
+void ptr() {
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
-			if (map[i][j] == '@') {
-				return true;
-			}
+			cout << map[i][j] << ' ';
 		}
+		cout << endl;
 	}
-	return false;
+	cout << "=============" << endl;
 }
 
-
-bool canGo(int x, int y) {
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (0 <= nx && nx < h && 0 <= ny && ny < w) {
-			if (map[nx][ny] == '.') { // 하나라도 빈공간이 있으면 이동 가능
-				return true;
+void bfs() {
+	while(!sang.empty()) {		
+		// 상근이가 탈출 가능한 위치인지?
+		/*for (int i = 0; i < sang.size(); i++) {
+			int sang_y = sang[i].first;
+			int sang_x = sang[i].second;
+			if (sang_y == 0 || sang_y == h - 1 || sang_x == 0 || sang_x == w - 1) {
+				cout << result + 1 << endl;
+				return;
 			}
-		}
-	}	
-	return false;
-}
-
-void movefire(){ // 벽을 제외하고 주변을 * 로 바꿈
-	int size = fire.size();
-	for (int i = 0; i < size; i++) {
-		int x = fire[0].first;
-		int y = fire[0].second;
-		fire.pop_front();
-		for (int k = 0; k < 4; k++) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			if (0 <= nx && nx < h && 0 <= ny && ny < w) {
-				if (map[nx][ny] == '.' ) { 
-					map[nx][ny] = '*';
-					fire.push_back({ nx,ny });
+		}*/
+		// 불 확산, 위치 저장
+		bool is_move = false;
+		int fire_size = fire.size();
+		for (int i = 0; i < fire_size; i++) {
+			int fire_y = fire.front().first;
+			int fire_x = fire.front().second;
+			fire.pop();
+			for (int k = 0; k < 4; k++) {
+				int fire_ny = fire_y + dy[k];
+				int fire_nx = fire_x + dx[k];
+				if (0 > fire_ny || fire_ny >= h || 0 > fire_nx || fire_nx >= w) continue;
+				if (map[fire_ny][fire_nx] != '#') { // 벽이 아니라면 불이 붙는다
+					map[fire_ny][fire_nx] = '*';
+					fire.push({ fire_ny,fire_nx }); // 새롭게 확산된 불의 위치 저장
 				}
 			}
-		}		
-	}
-
-}
-void bfs(int x, int y) {
-	queue<pair<int, int>> q;
-	q.push({ x,y });
-	while (!q.empty()) {
-		movefire();
-		x = q.front().first;
-		y = q.front().second;
-		q.pop();
-		int nx; int ny;
-		for (int i = 0; i < 4; i++) {
-			nx = x + dx[i];
-			ny = y + dy[i];
-			if (0 <= nx && nx < h && 0 <= ny && ny < w && map[nx][ny] == '.') {// 상근이가 이동 가능
-				// 이동할 때 다음에 불이 번질 곳은 가면 안됨. 상근이가 먼저 이동해야 함.
-				// 이동하는 모든 곳에 @를 넣어두자.
-				map[nx][ny] = '@';
-				q.push({ nx,ny });				
-				dist[nx][ny] = dist[x][y] + 1;
+		}
+		// 상근이 이동, 위치 저장
+		int sang_size = sang.size();
+		for (int i = 0; i < sang_size; i++) {
+			int y = sang.front().first;
+			int x = sang.front().second;
+			visit[y][x] = true;
+			sang.pop();
+			if (y == 0 || y == h - 1 || x == 0 || x == w - 1) {
+				cout << result + 1 << endl;
+				return;
 			}
-			
-		// 탈출 가능한지 확인 : 가장 자리에 상근이가 있으면 된다.
-		if (nx == 0 || nx == h-1 || ny == 0 || ny == w-1) {
-			cout << dist[nx][ny] + 1<<endl;
+			for (int k = 0; k < 4; k++) {
+				int ny = y + dy[k];
+				int nx = x + dx[k];
+				if (0 > ny || ny >= h || 0 > nx || nx >= w) continue; // 진행방향이 범위 초과
+				if (map[ny][nx] == '#' || map[ny][nx] == '*' || visit[ny][nx] == true) continue; // 진행방향이 벽, 불, 방문했던 위치			
+				map[ny][nx] = '@';
+				sang.push({ ny,nx });
+				is_move = true;
+				visit[ny][nx] = true;
+			}
+		}
+		result++;
+		//ptr();
+		if (!is_move) {
+			cout << "IMPOSSIBLE" << endl;
 			return;
 		}
-		}
 	}
 }
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
-	int t;
+	freopen("input.txt", "r", stdin);
+
 	cin >> t;
-	while (t--) {
-		int startX = 0; int startY = 0; // 상근이의 시작점
+	while (t--) {	
+		result = 0;
 		cin >> w >> h;
-		map.clear();  fire.clear();
-		map.resize(h); 
 		for (int i = 0; i < h; i++) {
-			string str;
+			string str; 
 			cin >> str;
 			for (int j = 0; j < w; j++) {
-				map[i].push_back(str[j]);
-				dist[i][j]=0;
-				if (str[j] == '@') {// 상근이의 위치
-					startX = i;
-					startY = j;
+				map[i][j] = str[j];
+				if (str[j] == '@') {
+					sang.push({ i,j });
 				}
-				else if (str[j] == '*') { // 불
-					fire.push_back({ i,j });
+				else if (str[j] == '*') {
+					fire.push({ i,j });
 				}
 			}
 		}
-
-		// 벽 or 불로 둘러쌓인지 확인
-		if (!canGo(startX, startY)) {
-			cout << "IMPOSSIBLE" << endl;
-		}
-		 //	상근이, 불 이동
-		else {			
-			bfs(startX, startY);
-		}		
+		bfs();
+		// 초기화
+		while (!sang.empty())sang.pop(); 
+		while (!fire.empty())fire.pop();
+		memset(visit, false, sizeof(visit));
+		memset(map, '#', sizeof(map));
 	}
-
 }
