f5b9c7901e9089200db3837e0744a1c1ddeb10ef
mk <midaslmg94@gmail.com>
2020-03-03 17:28:30 +0900

    이분탐색(파라메트릭서치) -(실패, 스터디원 도움) 파라메트릭 서치 적용하기 가어려웠다

---
+++ 코드 템플릿.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<string.h>
+#define endl "\n"
+using namespace std;
+
+int main() {	
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	freopen("input.txt", "r", stdin);
+	
+	return 0;
+}
