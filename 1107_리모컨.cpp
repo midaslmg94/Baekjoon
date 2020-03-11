b47b0dd050dfb3e4bb959e69ddbbc7548588bf1d
mk <midaslmg94@gmail.com>
2020-03-11 23:21:52 +0900

    이분 탐색(실패) - 이 문제를 이분탐색으로 풀어야 할 지도 몰랐고, 어떤 식으로 이분탐색을 해야할 지 모르겠다.

--- 1107_리모컨.cpp
+++ 1107_리모컨.cpp
@@ -8,58 +8,21 @@
 #include<string.h>
 #define endl "\n"
 using namespace std;
-string str;
-bool canUse[10];
+int n;
 int m;
+bool broken[10];
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
-	cin >> str;
-	cin >> m;
-	memset(canUse, true, sizeof(canUse));
+	cin >> n>> m;
 	for (int i = 0; i < m; i++) {
-		int tmp; cin >> tmp;
-		canUse[tmp] = false;
+		int ch;
+		cin >> ch;
+		broken[ch] = true;
 	}
+	int cur = abs(100 - n); // 현재 채널에서 +,-버튼으로 움직여서 갈 수 있는 경우
 
-	vector<char>num;
-	for (int i = 0; i < str.size(); i++) {
-		int n = str[i] - '0';
-		int idx = 1;
-		while (true) {
-			if (canUse[n] == true) {
-				num.push_back(n + 48); // int를 char로 변환
-				break;
-			}
-			else {
-				if (n + idx <= 9) {
-					if (canUse[n + idx]) { // 숫자 한개 더 큰거 확인
-						num.push_back(n + idx + 48);
-						break;
-					}
-				}
-				if (n - idx >= 0) {
-					if (canUse[n - idx]) { // 숫자 하나 더 작은거 확인
-						num.push_back(n - idx + 48);
-						break;
-					}
-				}
-			}
-			idx++;
-		}
-	}
 
-	string s = "";
-	for (int i = 0; i < num.size(); i++) {
-		s += num[i];
-	}
-	int candi = stoi(s);
-	int curChannel = 100;
 
-	int curToTarget = abs(curChannel - stoi(str));
-	int candiToTarget = abs(candi - stoi(str)) + num.size();
-	int result = 0;
-	result = min(curToTarget, candiToTarget);
-	cout << result;
 	return 0;
 }
