bc787759df15b923f1f00f95dcf0ba8e8cc3b6b9
mk <midaslmg94@gmail.com>
2020-03-12 23:14:29 +0900

    우선순위 큐 - 주석 추가

--- 2075_N번째 큰 수.cpp
+++ 2075_N번째 큰 수.cpp
@@ -12,15 +12,16 @@ int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
 	cin >> n;
-	priority_queue<int, vector<int>,greater<int>>pq;	
+	priority_queue<int, vector<int>,greater<int>>pq; // 최소힙으로 구현 
+
 	int tmp;
 	for (int i = 0; i < n * n; i++) {
 		cin >> tmp;
-		if (pq.size() < n) {
+		if (pq.size() < n) { // 입력 값 중 크기가 큰 N개의 값 저장. 최소힙이므로 N번째 큰 값이 top에 위치함
 			pq.push(tmp);
 		}
 		else {			
-			if (pq.top() < tmp) { // 더 큰 수가 들어오면
+			if (pq.top() < tmp) { // pq에 있는 값 중 가장 작은 값 보다 더 큰 수가 들어오면, pop후 push
 				pq.pop(); 
 				pq.push(tmp);
 			}
@@ -28,12 +29,8 @@ int main() {
 				continue;
 			}
 		}
-		cout << pq.top() << endl; // 젤 작은거
 	}
-	//while (!pq.empty()) {
-	//	cout << pq.top()<<endl;
-	//	pq.pop();
-	//}
+	cout << pq.top() << endl; // 젤 작은거
 	
 	return 0;
 }
