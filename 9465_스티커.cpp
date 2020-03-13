e1e35a7c178190c0f7fb06b04b621a5311327cf8
mk <midaslmg94@gmail.com>
2020-03-13 22:53:26 +0900

    DP(실패) - 접근을 방법을 잘 모르겠음

--- 9465_스티커.cpp
+++ 9465_스티커.cpp
@@ -1,41 +1,34 @@
 #include<iostream>
 #include<vector>
+#include<string.h>
 #include<algorithm>
+#define MAX 100000
 using namespace std;
 int t;
 int n;
-long long score;
-
-vector < pair<int, int>>check_up = { {0,-1},{-1,0},{0,1} }; // 왼쪽, 위쪽, 오른쪽 확인
-vector < pair<int, int>>check_down = { {0,-1},{1,0},{0,1} }; // 왼쪽, 아래쪽, 오른쪽 확인
-
-
-vector<vector<int>>v;
+int map[2][MAX];
+int dp[2][MAX];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
 	cin >> t;
 	while (t--) {
+		memset(map, 0, sizeof(map));
+		memset(dp, 0, sizeof(dp));
 		cin >> n;
 		for (int i = 0; i < 2; i++) {
-			vector<int>tmp(n);
 			for (int j = 0; j < n; j++) {
-				cin >> tmp[j];
+				cin >> map[i][j];
 			}
-			v.push_back(tmp);
 		}
-
-		/*3개씩 묶어서 확인해보자*/
-		int sum = 0;
-		int start = 0; // 0 또는 1의 값을 갖는다.--> 2n 배열에서 위, 아래 표시
-		for (int i = 0; i < n; i += 3) {
-			int one = max(v[start][i] + v[!start][i + 1] + v[start][i + 2], v[!start][i] + v[start][i + 1] + v[!start][i + 2]);
-			int two = max(v[start][i] + v[start][i + 2], v[start][i] + v[!start][i+2]);
-			int three = max(v[!start][i] + v[start][i + 2], v[!start][i] + v[!start][i + 2]);
-			sum += max(one, max(two, three));
+		dp[0][0] = map[0][0];
+		dp[0][1] = map[1][0];
+		for (int i = 1; i < n; i++) {
+			dp[0][i] = ;
+			dp[1][i] = ;
 		}
-		cout << sum << "\n";
-		v.clear();
 	}
+	return 0;
+
 }
