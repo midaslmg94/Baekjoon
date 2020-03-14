f4811f63ac30361fa721c3efd48a126488878d44
mk <midaslmg94@gmail.com>
2020-03-14 23:04:19 +0900

    DP(실패) - 어떻게 dp로 해야될 지 모르겠다

--- 12019_동아리방 청소!.cpp
+++ 12019_동아리방 청소!.cpp
@@ -8,53 +8,13 @@
 #define MAX 100+1
 #define INF 987654231
 using namespace std;
-int dp[2][MAX];
 int people[MAX];
 bool clean[MAX];
 int answer = INF;
 vector<int>clean_day;
 vector<int>day;
 int n, m;
-int calc() {
-	int dirty[MAX]; // 더러움(아침)
-	int unpleasant = 0; // 당일의 불쾌함
-	int result = 0; // 누적 불쾌함
-	memset(dirty, 0, sizeof(dirty));
-	for (int i = 1; i <= n; i++) {
-		if (clean[i]) { // 청소를 한 날
-			dirty[i] = 0;
-		}
-		else {
-			dirty[i] = dirty[i - 1] + people[i - 1];
-		}
-		unpleasant = dirty[i] * people[i];
-		result += unpleasant;
-	}
-	return result;
-}
-void dfs(int idx, int cnt) {
-	if (cnt == m) {
-		day.clear();
-		for (int i = 1; i <= n; i++) {
-			if (!clean[i])continue;
-			day.push_back(i); // 청소하는 날			
-		}
-		if (answer > calc()) {
-			answer = calc();
-			clean_day.clear();
-			for (int i = 0; i < day.size(); i++) {
-				clean_day.push_back(day[i]-1);
-			}
-		}
-		return;
-	}
-	for (int i = idx; i <= n; i++) {
-		if (clean[i])continue;
-		clean[i] = true;
-		dfs(i + 1, cnt + 1);
-		clean[i] = false;
-	}
-}
+
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
@@ -62,12 +22,9 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		cin >> people[i];
 	}
-	dfs(1, 0);
+	
 
-	cout << answer << endl;
-	sort(clean_day.begin(), clean_day.end());
-	for (int i = 0; i < clean_day.size(); i++) {
-		cout << clean_day[i] << ' ';
-	}
+	
+
 	return 0;
 }
