dbc5cbba6b2796089e1cbf27a1307cefa4871847
mk <midaslmg94@gmail.com>
2020-03-20 00:16:19 +0900

    BFS, 이분탐색(블로그 참조)- 이해 안가던 부분 주석 수정

--- 1939_중량제한.cpp
+++ 1939_중량제한.cpp
@@ -12,7 +12,7 @@ vector<pair<int, int>>map[MAX]; // 연결노드, 비용
 bool visit[MAX];
 int start_fatctory;
 int finish_factory;
-bool bfs(int start, int limitWeight) { // 해당 무게로 시작 공장에서 최종 공장까지 갈 수 있는가
+bool bfs(int start, int cost) { // 해당 무게로 시작 공장에서 최종 공장까지 갈 수 있는가
 	queue<int>q;
 	q.push(start);
 	visit[start] = true;
@@ -26,7 +26,7 @@ bool bfs(int start, int limitWeight) { // 해당 무게로 시작 공장에서 최종 공장까�
 		for (int i = 0; i < map[cur].size(); i++) {
 			int next = map[cur][i].first;
 			int nextCost = map[cur][i].second;
-			if (!visit[next] && limitWeight <= nextCost) { // 왜 <= 인것인지
+			if (!visit[next] && cost <= nextCost) { // 해당 무게가 지나갈 수 있는지 확인
 				visit[next] = true;
 				q.push(next);
 			}
