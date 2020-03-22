4e5c101a08c6a0fcf380d8911edc03fd262802fb
mk <midaslmg94@gmail.com>
2020-03-23 00:45:49 +0900

    BFS(블로그 참조) - dist배열을 3차원으로 관리하는 것 까지는 생각을 하고, 구현을 했는데 세세한 부분(방문 처리 등)을 구현하지 못해서 블로그를 참조하였다

--- 2206_벽 부수고 이동하기.cpp
+++ 2206_벽 부수고 이동하기.cpp
@@ -1,89 +1,63 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 #include<queue>
+#include<deque>
 #include<string.h>
+#define endl "\n"
 #define MAX 1000
 #define INF 99
+#define pii pair<pair<int,int>,int>
 using namespace std;
-struct INFO
-{
-	int dist;
-	bool isBreak;
-};
-
+int map[MAX][MAX];
+int dist[MAX][MAX][2]; // i,j까지 오는데 걸린거리, 벽을 부쉈는지 안부쉈는지(부쉈으면 0, 안부쉈으면 1)
+int dy[4] = { -1,1,0,0 };
+int dx[4] = { 0,0,-1,1 };
 int n, m;
-int map[MAX][MAX];//  입력받은 원본
-INFO dist[MAX][MAX];// 거리
-bool visit[MAX][MAX];
-int dy[4] = { 0,0,-1,1 };
-int dx[4] = { 1,-1,0,0 };
+int answer = INF;
 
-void bfs(int y, int x, bool flag) {//(0,0)부터시작	
-	queue<pair<int, int>>q;
-	q.push({ y,x });
-	visit[y][x] = true;
-	dist[y][x].dist = 1;
-	dist[y][x].isBreak = flag;
+int bfs() {
+	queue<pii>q;
+	q.push(make_pair(make_pair(0,0),1)); // 0,0 1
+	dist[0][0][1] = 1; // 시작하는 칸은 빈 공간
 	while (!q.empty()) {
-		y = q.front().first;
-		x = q.front().second;
-		flag = dist[y][x].isBreak;
+		int y = q.front().first.first;
+		int x = q.front().first.second;
+		int block = q.front().second;
+		if (y == n - 1 && x == m - 1) {
+			return dist[y][x][block];
+		}
 		q.pop();
 		for (int k = 0; k < 4; k++) {
-			int ny = dy[k] + y;
-			int nx = dx[k] + x;
-			if (0 <= ny && ny < n && 0 <= nx && nx < m && !visit[ny][nx]) {
-				if (map[ny][nx] == 1 && flag == true)// 벽이면서, 부쉈음
-					continue;
-				if (map[ny][nx] == 1 && flag == false) { // 벽이지만, 아직 안부심
-					dist[ny][nx].dist = min(dist[ny][nx].dist,dist[y][x].dist + 1);
-					dist[ny][nx].isBreak = true;					
+			int ny = y + dy[k];
+			int nx = x + dx[k];
+			if (0 > ny || ny >= n || 0 > nx || nx >= m)continue; // 범위 초과			
+			if (map[ny][nx] == 1) { // 벽을 만남
+				if (block == 1) { // 아직 벽을 안부순 상태
+					dist[ny][nx][0] = dist[y][x][1] + 1;
+					q.push(make_pair(make_pair(ny, nx),0));
 				}
-				if(map[ny][nx] == 0) { // 빈곳
-					dist[ny][nx].dist = min(dist[ny][nx].dist, dist[y][x].dist + 1);
-					dist[ny][nx].isBreak = flag;
-				}				
-				visit[ny][nx] = true;
-				q.push({ ny,nx });
 			}
-		}
-		cout << endl;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				cout << dist[i][j].dist << ' ';
+			else if (map[ny][nx] == 0) { // 빈 공간
+				if (dist[ny][nx][block] == 0) { // 방문을 안했다면
+					dist[ny][nx][block] = dist[y][x][block] + 1;
+					q.push(make_pair(make_pair(ny, nx), block));
+				}
 			}
-			cout << endl;
-		}
-		cout << endl;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				cout << dist[i][j].isBreak << ' ';
-			}
-			cout << endl;
 		}
 	}
+	return -1;
 }
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
 	cin >> n >> m;
-	int tmp;
 	for (int i = 0; i < n; i++) {
-		string str; // 입력이 공백없이 들어옴
-		cin >> str;
+		string str; cin >> str;
 		for (int j = 0; j < m; j++) {
-			tmp = str[j] - '0';
-			map[i][j] = tmp;
-			dist[i][j].dist = INF;
-			dist[i][j].isBreak = false;
+			map[i][j] = str[j] - '0';
 		}
 	}
-
-	bfs(0, 0, false);
-	if (dist[n - 1][m - 1].dist == INF)
-		cout << -1;
-	else
-		cout << dist[n - 1][m - 1].dist;
+	cout << bfs();
+	return 0;
 }
