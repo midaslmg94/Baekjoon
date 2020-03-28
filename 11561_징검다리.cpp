76006278fa22ed233af275a1c777a93c06a12f32
mk <midaslmg94@gmail.com>
2020-03-28 23:41:49 +0900

    이분탐색(블로그 참조) - mid 값을 구하기 위한 공식을 블로그에서 참조했다. 숫자의 범위가 커서 unsigned long long을 사용했다

--- 11561_징검다리.cpp
+++ 11561_징검다리.cpp
@@ -1,32 +1,30 @@
-#include<iostream>
+#include <bits/stdc++.h>
+#define ll unsigned long long
 using namespace std;
 int t;
-long long n; // 1번, 2번 ... n번 징검다리
+ll n; // 1번, 2번 ... n번 징검다리
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);	cout.tie();
 	freopen("input.txt", "r", stdin);
 	cin >> t;
-	/*
-	첫 징검다리는 점프해서 아무 것이나 밟을 수 있다. 이 점프가 첫 점프이다.
-	두 번째 점프부터는 이전에 점프한 거리보다 1 이상 더 긴 거리를 뛰어야만 한다.
-	N번 징검다리는 반드시 밟아야 한다.
-	N번 징검다리를 밟은 후 강 건너로 이동할 땐 점프를 하지 않으므로 위의 규칙이 적용되지 않는다.
-	*/
-
 	while (t--) {
-		long long jump = 1; // 시작점에서 1로 갔으므로 초기값:1
-		cin >> n;
-		long long cur = 1; // 1번부터 뛰어야 함
-		long long dist = 1;
-		while (cur!=n) {
-			dist++;
-			cur = cur + dist;
-			if (cur > n) {
-				break;
+		cin >> n;		
+		ll left = 0;
+		ll right = n;
+		ll answer = 0;
+		while (left <= right) {
+			ll mid = (left + right) / 2; // 개수 
+			ll cnt = (mid + 1) * mid / 2; // // 등차수열의 합 공식
+			if (cnt<=n) {
+				left = mid + 1;
+				answer = mid;
 			}
-			jump++;
+			else {
+
+				right = mid - 1;
+			}
 		}
-		cout << jump<<"\n";
+		cout << answer << endl;
 	}
 }
