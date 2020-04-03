ae0e4759a11a13c771c40fab816e08e5ae2485d6
mk <midaslmg94@gmail.com>
2020-04-03 14:32:10 +0900

    이분탐색 - 처음에 출력을 잘못해서 틀렸었다

--- 2805_나무 자르기.cpp
+++ 2805_나무 자르기.cpp
@@ -1,34 +1,38 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
+#define endl "\n"
+#define MAX 1000000
+#define ll long long 
 using namespace std;
-int tree[1000000];
+int n, m;
+int tree[MAX];
 int main() {
-	int N, M;
-	cin >> N >> M;
-	int maxTree = 0;
-	for (int i = 0; i < N; i++) {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	freopen("input.txt", "r", stdin);
+	cin >> n >> m;
+	int left = 0;
+	int right = 0;
+	for (int i = 0; i < n; i++) {
 		cin >> tree[i];
-		maxTree = max(maxTree, tree[i]);
+		right = max(right, tree[i]);
 	}
-	long long left = 0; // 가장 작은 나무의 길이
-	long long right = maxTree; // 가장 큰 나무의 길이
-	int answer = 0;
-	long long myTree = 0; // 현재 자른 나무들의 길이
+	ll answer = 0;
 	while (left <= right) {
-		long long mid = (left + right) / 2;
-		myTree = 0;
-		for (int i = 0; i < N; i++) {
+		int mid = (left + right) / 2; // 절단기의 높이
+		ll res = 0;
+		for (int i = 0; i < n; i++) {
 			if (tree[i] > mid) {
-				myTree += tree[i] - mid;
-			} 
+				res += tree[i] - mid;
+			}
 		}
-		if (M > myTree) { // 나무가 더 필요하다면
+		if (res>= m) { // 적어도 M미터의 나무보다 더 많이 자름 -> 절단기의 높이를 늘림
+			answer = mid;
+			left = mid + 1;
+		}
+		else {
 			right = mid - 1;
 		}
-		else { // 나무를 너무 많이 자름
-			left = mid + 1;
-			answer = mid;
-		}
+
 	}
 	cout << answer;
+	return 0;
 }
