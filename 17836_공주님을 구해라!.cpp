1deb352bb469e0663cda35d6b687e518ab7f272c
mk <midaslmg94@gmail.com>
2020-04-03 20:21:40 +0900

    시뮬레이션(BFS) - 검을 가지고 있는 지 상태여부를 3차원으로 표현하는게 중요

--- 17836_공주님을 구해라!.cpp
+++ 17836_공주님을 구해라!.cpp
@@ -1,83 +1,12 @@
 #include<bits/stdc++.h>
 #define endl "\n"
-#define MAX 101
-#define INF 9876
+#define MAX 100
 using namespace std;
-struct INFO {
-	int y; // y좌표
-	int x; // x좌표
-	bool isSword; // 검 획득 여부
-};
-
-struct VISIT {
-	int take_time; // 해당위치까지 오는데 걸린 시간
-	bool isSword; // 검 획득 여부
-};
-int n, m, limit_time;
 int castle[MAX][MAX];
-VISIT visit[MAX][MAX];
+int visit[MAX][MAX][2]; // i,j번째 위치에서 칼이 없으면 0에, 칼이 있으면 1에 값 저장
 int dy[4] = { -1,1,0,0 };
-int dx[4] = { 0,0,1,-1 };
-int res = INF;
-void prt() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cout << visit[i][j].take_time << ' ';
-		}
-		cout << endl;
-	}
-	cout << endl; cout << endl;
-}
-int bfs(int y, int x, bool sword) {
-	queue<INFO>q;
-	q.push({ y, x, sword });
-	visit[y][x].take_time = 0;
-	visit[y][x].isSword = false;
-	while (!q.empty()) {
-		y = q.front().y;
-		x = q.front().x;
-		sword = q.front().isSword;
-		q.pop();
-		prt();
-		if (y == n - 1 && x == m - 1) { // 공주를 찾음
-			if (visit[y][x].take_time <= limit_time)
-				return visit[y][x].take_time;
-			else
-				return INF;
-		}
-		//cout << " y:" << y << " x:" << x << " sword:" << sword << endl;
-		for (int k = 0; k < 4; k++) {
-			int ny = y + dy[k];
-			int nx = x + dx[k];
-			if (0 > ny || ny >= n || 0 > nx || nx >= m)continue;
-			if (visit[ny][nx].take_time != 0)continue;
-
-			if (sword == true) { // 현재 칼을 갖고 있음
-				if (castle[ny][nx] == 0 || castle[ny][nx] == 1) {
-					visit[ny][nx].take_time = visit[y][x].take_time + 1;
-					visit[ny][nx].isSword = true;
-					q.push({ ny,nx,true });
-				}
-			}
-			else { // 현재 칼이 없음
-				if (castle[ny][nx] == 0) {
-					visit[ny][nx].take_time = visit[y][x].take_time + 1;
-					q.push({ ny,nx,visit[ny][nx].isSword });
-				}
-				else if (castle[ny][nx] == 1) { // 못감
-					continue;
-				}
-				else { // 칼을 찾음
-					visit[ny][nx].take_time = visit[y][x].take_time + 1;
-					visit[ny][nx].isSword = true;
-					q.push({ ny,nx,true });
-				}
-			}
-		}
-	}
-	return INF;
-}
-
+int dx[4] = { 0,0,-1,1 };
+int n, m, limit_time;
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
@@ -85,16 +14,57 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> castle[i][j];
-			visit[i][j].isSword = false;
-			visit[i][j].take_time = 0;
+			visit[i][j][0] = -1;
+			visit[i][j][1] = -1;			
 		}
 	}
-	int answer = min(res, bfs(0, 0, false));
-	if (answer == INF) {
+	queue<pair<pair<int, int>,int>>q; // y좌표,x좌표,칼이 있는지 없는지
+	q.push({ { 0,0 },0 });
+	visit[0][0][0] = 0;
+	int t = 0;
+	int answer = 987654;
+	while (t <= limit_time) {
+		int q_size = q.size();
+		for (int i = 0; i < q_size; i++) {
+			int y = q.front().first.first;
+			int x = q.front().first.second;
+			int sword = q.front().second;
+			q.pop();
+			if (y == n - 1 && x == m - 1) { // 공주 찾음
+				answer = min(answer, visit[y][x][sword]);
+			}
+			for (int k = 0; k < 4; k++) {
+				int ny = y + dy[k];
+				int nx = x + dx[k];
+				if (0 > ny || ny >= n || 0 > nx || nx >= m)continue;
+				if (visit[ny][nx][sword] != -1)continue;
+				if (castle[ny][nx] == 0) { // 빈 공간
+					if (sword == 1) { // 현재 칼이 있음
+						q.push({ { ny,nx },1 });
+						visit[ny][nx][sword] = visit[y][x][sword] + 1;		
+					}
+					else {						
+						q.push({ { ny,nx },0 }); // 현재 칼이 없음
+						visit[ny][nx][sword] = visit[y][x][sword] + 1;		
+					}
+				}
+				else if (castle[ny][nx] == 1) { // 벽
+					if (sword == 1) { // 칼이 있는 경우에만 진행 가능
+						q.push({ { ny,nx },1 });
+						visit[ny][nx][sword] = visit[y][x][sword] + 1;
+					}
+				}
+				else { // 칼
+					q.push({ { ny,nx },1 });
+					visit[ny][nx][1] = visit[y][x][sword] + 1;
+				}
+			}
+		}
+		t++;
+	}
+	if (answer == 987654)
 		cout << "Fail";
-	}
-	else {
+	else
 		cout << answer;
-	}
 	return 0;
 }
