16e88becdb3bd665617512bc8b7a879a304b0658
mk <midaslmg94@gmail.com>
2020-04-29 23:10:31 +0900

    시뮬레이션(실패) - 다시 도전했지만, 실패했다. 블로그를 보니 접근은 비슷하게 한거 같은데 사소한 부분이 틀린듯

--- 실패/17143_낚시왕.cpp
+++ 실패/17143_낚시왕.cpp
@@ -1,77 +1,96 @@
-#include<iostream>
-#include<string.h>
-#include<vector>
-#define MAX 101
+#include <bits/stdc++.h>
+#define endl "\n"
+#define MAX 100 + 1
 using namespace std;
-int dy[4] = { 0,0,1,-1 };
-int dx[4] = { 1,-1,0,0 };
-int sum; // 잡은상어의 총 크기
-struct INFO {
-	int speed;
-	int dir;
-	int size;
+struct SHARK {  // 상어의 정보
+    int y;
+    int x;
+    int s;  // 속력
+    int d;  // 방향
+    int z;  // 크기
 };
-INFO shark[MAX][MAX];
-vector<bool>visit[MAX][MAX];
-int R, C, M;// 세로, 가로, 상어 수
-
-void catchShark(int pos) { //  낚시왕이 상어를 잡음
-	for (int i = 1; i <= R; i++) {
-		if (shark[i][pos].size) {// 상어가 있으면
-			sum += shark[i][pos].size;
-			shark[i][pos] = { 0,0,0 };
-			return;
-		}
-	}
+struct INFO {  // 현재 위치에 있는 상어의 정보
+    int idx;   // 몇번 상어인지
+    int size;  // 상어의 크기
+};
+int r, c, m;
+deque<INFO> board[MAX][MAX];  // 보드판의 정보
+vector<SHARK> shark;          // 상어의 정보
+int answer = 0;
+int y, x, s, d, z;
+int dy[] = {0, -1, 1, 0, 0};  // 상,하,좌,우
+int dx[] = {0, 0, 0, 1, -1};
+bool cmp(INFO a, INFO b) {
+    if (a.size > b.size) return true;
+    return false;
 }
-void moveShark() { // 상어가 움직임, 움직인 상어를 체크
-	// 같은 공간에 2마리 이상의 상어가 있을 때 처리
-	for (int i = 1; i <= R; i++) {
-		for (int j = 1; j <= C; j++) {
-			if (shark[i][j].size) {//상어가 있으면
-				// 경계선에 있는 상어일 경우, 반대쪽으로 방향을 돌려줌
-
-				// 상어가 움직임
-				int cur_dir = shark[i][j].dir;
-				int cur_speed = shark[i][j].speed;
-				if (cur_dir == 1) { // R과 연관
-					
-				}
-				else if(cur_dir == 2) { // R과 연관
-
-				}
-				else if (cur_dir == 3) { // C와 연관
-
-				}
-				else { // C와 연관
-
-				}
-			}
-
-		}
-	}
+void prt() {
+    for (int i = 0; i < shark.size(); i++) {
+        cout << shark[i].y << ' ' << shark[i].x << ' ' << shark[i].s << ' ' << shark[i].d << ' ' << shark[i].z << ' ';
+        cout << endl;
+    }
+    cout << endl
+         << endl;
 }
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);	cout.tie();
-	freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("input.txt", "r", stdin);
+    cin >> r >> c >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> y >> x >> s >> d >> z;
+        shark.push_back({y, x, s, d, z});
+        board[shark[i].y][shark[i].x].push_back({i, shark[i].z});  // {i번 상어, 크기}
+    }
+    for (int turn = 1; turn <= c; turn++) {  // 낚시왕이 한 칸 이동
+        for (int i = 1; i <= r; i++) {       // 낚시왕이 상어를 잡음 -> 상어가 사라짐
+            if (board[i][turn].size() != 0) {
+                answer += board[i][turn].front().size;    // 상어의 크기만큼 추가
+                shark[board[i][turn].front().idx].z = 0;  // 크기가 0이면 죽은 것
+                board[i][turn].front().size = 0;          // 상어 삭제
+                break;
+            }
+        }
+        // 상어 이동
+        for (int j = 0; j < m; j++) {
+            if (shark[j].z == 0) continue;
+            int y = shark[j].y;
+            int x = shark[j].x;
+            int dir = shark[j].d;
+            int speed = shark[j].s;
+            board[y][x].pop_front();  // 해당 위치에서 상어 뺌
+            if (dir == 1 || dir == 2) {
+                int rotate = (r - 1) * 2;  // 이 만큼 지나면 원래 방향을 가지고 원래 자리로 돌아옴
+                if (speed >= rotate) speed = speed % rotate;
+                for (int k = 0; k < speed; k++) {
+					int ny = y + dy[dir];
+					int nx = x + dx[dir];
+					
+				}
 
-	cin >> R >> C >> M;
-	int r, c, s, d, z;
-	for (int i = 0; i < M; i++) {
-		cin >> r >> c >> s >> d >> z; // d가 1인 경우는 위, 2인 경우는 아래, 3인 경우는 오른쪽, 4인 경우는 왼쪽
-		shark[r][c].speed = s;
-		shark[r][c].dir = d;
-		shark[r][c].size = z;		
-	}
-	for (int i = 1; i <= C; i++) {
-		int pos = i; // 낚시왕의 위치
-		catchShark(pos);
-		moveShark();
-		memset(visit, false, sizeof(visit));
-		
-	}
-
-
-	cout << sum;
+            } else {
+            }
+            // 새 위치로 변경
+            shark[j].y = y;
+            shark[j].x = x;
+            shark[j].d = dir;
+            board[y][x].push_back({j, shark[j].z});
+            //prt();
+        }
+        // 같은 칸에 2마리 이상의 상어 -> 크기가 작은 상어는 다 죽음
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if (board[i][j].size() > 1) {
+                    sort(board[i][j].begin(), board[i][j].end(), cmp);
+                    // 한마리 남을때 까지 뺌
+                    while (board[i][j].size() != 1) {
+                        board[i][j].pop_back();
+                    }
+                }
+            }
+        }
+    }
+    cout << answer;
+    return 0;
 }
