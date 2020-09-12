355024d61f76d06e00d9dba5cb8b2821136cd4a2
mk <midaslmg94@gmail.com>
2020-09-13 02:29:55 +0900

    시뮬레이션 : goto를 써서 좋은 코드는 아니지만..

--- Simulation/16113_시그널.cpp
+++ Simulation/16113_시그널.cpp
@@ -1,80 +1,67 @@
-#include<iostream>
-#include<queue>
+#include<bits/stdc++.h>
+#define endl "\n"
+#define MAX 10
 using namespace std;
 int n;
-char arr[5][20000];
-bool visit[5][20000];
-char shape[5][3];
-int dy[4] = { 0,0,1,-1 };
-int dx[4] = { 1,-1,0,0 };
-char zero[5][3] = {
-	'#','#','#',
-	'#','.', '#',
-	'#','.', '#',
-	'#','.', '#',
-	'#','#','#'
-};
-char one[5][1] = {
-	'#',
-	'#',
-	'#',
-	'#',
-	'#'
-};
-char three[5][3] = {
-	'#','#','#',
-	' ',' ', '#',
-	'#','#','#',
-	' ',' ', '#',
-	'#','#','#'
-};
-char seven[5][3] = {
-	'#','#','#',
-	' ',' ', '#',
-	' ',' ','#',
-	' ',' ','#',
-	' ',' ','#'
-};
-void bfs(int y, int x, int row, int col) {
-	queue<pair<int, int>>q;
-	q.push({ y,x });
-	visit[y][x] = true;
-	while (!q.empty()) {
-		y = q.front().first;
-		x = q.front().second;
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (0 <= ny && ny < col && 0 <= nx && nx < row && arr[ny][ny] == '#' && visit[ny][nx] == false) {
-				visit[ny][nx] = true;
-				q.push({ ny, nx });
-			}
-		}
-	}
-}
-
-
-
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);	cout.tie();
+string sign;
+vector<int>ans;
+// 해당 숫자를 위에서 아래로 읽었을 때 나오는 값
+string zero = "######...######";
+string one = "#####";
+string two = "#.####.#.####.#";
+string three = "#.#.##.#.######";
+string four = "###....#..#####";
+string five = "###.##.#.##.###";
+string six = "######.#.##.###";
+string seven = "#....#....#####";
+string eight = "######.#.######";
+string nine = "###.##.#.######";
+char SIGNAL[5][20000];
+int main() {	
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	freopen("input.txt", "r", stdin);
-	cin >> n;
-	int row = n / 5;
-	int col = 5;
-	for (int i = 0; i < col; i++) {
-		for (int j = 0; j < row; j++) {
-			char tmp; cin >> arr[i][j];
-		}
-	}
-
-	for (int i = 0; i < col; i++) {
-		for (int j = 0; j < row; j++) {
-			if (arr[i][j] == '#' && !visit[i][j]) {
-				bfs(i, j, row, col);
-			}
-		}
-	}
+    cin>>n>>sign;
+    int len = n/5; // 몇개씩 끊어 읽을 건지
+    int idx = 0;
+    for(int i=0; i<5; i++){
+        for(int j=0; j<len; j++){            
+            SIGNAL[i][j] = sign[idx++];
+        }
+    }
+    
+    string str="";
+    for(int i=0; i<len; i++){        
+        if(SIGNAL[0][i]=='.'){  // 맨 윗줄이 공백인 경우 : 띄어쓰기 칸 이거나 숫자 4
+            if(str.compare("###..")==0) goto here;                       
+            else if(str.compare(zero)==0)cout<<'0';
+            else if(str.compare(one)==0)cout<<'1';
+            else if(str.compare(two)==0) cout<<'2';
+            else if(str.compare(three)==0) cout<<'3';
+            else if(str.compare(four)==0)cout<<'4';
+            else if(str.compare(five)==0)cout<<'5';
+            else if(str.compare(six)==0) cout<<'6';
+            else if(str.compare(seven)==0)cout<<'7';
+            else if(str.compare(eight)==0)cout<<'8';
+            else if(str.compare(nine)==0)cout<<'9';         
+            //ans.push_back(res);
+            str="";
+            continue;
+        }
+        here:
+        for(int j=0; j<5; j++){
+            str+=SIGNAL[j][i];
+        }        
+    } 
+    // 마지막에 숫자 붙어있는 경우
+    if(str.compare(zero)==0)cout<<'0';
+    else if(str.compare(one)==0)cout<< '1';
+    else if(str.compare(two)==0) cout<<'2';
+    else if(str.compare(three)==0) cout<<'3';
+    else if(str.compare(four)==0)cout<<'4';
+    else if(str.compare(five)==0)cout<<'5';
+    else if(str.compare(six)==0) cout<<'6';
+    else if(str.compare(seven)==0)cout<<'7';
+    else if(str.compare(eight)==0)cout<<'8';
+    else if(str.compare(nine)==0)cout<<'9';         
+	return 0;
 }
